8f7a2b82ffc5f6c7f311d78db3aeaab69c1ffdfd
HirokenOvo <526420278@qq.com>
2024-02-14 19:50:55 +0800

    add Socket

---
+++ src/net/Socket.cc
@@ -0,0 +1,72 @@
+#include "include/Socket.h"
+#include "include/InetAddress.h"
+#include "include/Logger.h"
+
+#include <netinet/tcp.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+Socket::Socket(int sockfd)
+    : sockfd_{sockfd}
+{
+}
+
+Socket ::~Socket() { close(sockfd_); }
+
+int Socket::getFd() const { return sockfd_; }
+
+void Socket::bindAddress(const InetAddress &localaddr)
+{
+    if (bind(sockfd_, (sockaddr *)localaddr.getSockAddr(), sizeof(sockaddr_in)))
+        LOG_FATAL("bind sockfd:%d fail \n", sockfd_);
+}
+
+void Socket::listen()
+{
+    if (::listen(sockfd_, 1024))
+        LOG_FATAL("listen sockfd:%d fail \n", sockfd_);
+}
+
+int Socket::accept(InetAddress *peeraddr)
+{
+    sockaddr_in addr;
+    socklen_t addrlen = sizeof addr;
+    bzero(&addr, sizeof addr);
+
+    int connfd = ::accept4(sockfd_, (sockaddr *)&addr, &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    if (connfd >= 0)
+        peeraddr->setSockAddr(addr);
+
+    return connfd;
+}
+
+void Socket::shutdownWrite()
+{
+    if (::shutdown(sockfd_, SHUT_WR) < 0)
+        LOG_ERROR("shutdownWrite error");
+}
+
+void Socket::setTcpNoDelay(bool on)
+{
+    int opt = on;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof opt);
+}
+
+void Socket::setReuseAddr(bool on)
+{
+    int opt = on;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof opt);
+}
+void Socket::setReusePort(bool on)
+{
+    int opt = on;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof opt);
+}
+void Socket::setKeepAlive(bool on)
+{
+    int opt = on;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof opt);
+}
---
+++ src/net/include/Socket.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "uncopyable.h"
+
+class InetAddress;
+
+class Socket : uncopyable
+{
+public:
+    explicit Socket(int sockfd);
+    ~Socket();
+
+    int getFd() const;
+    void bindAddress(const InetAddress &localaddr);
+    void listen();
+    int accept(InetAddress *peeraddr);
+
+    void shutdownWrite();
+
+    void setTcpNoDelay(bool on);
+    void setReuseAddr(bool on);
+    void setReusePort(bool on);
+    void setKeepAlive(bool on);
+
+private:
+    const int sockfd_;
+};
